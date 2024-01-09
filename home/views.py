9e608f25975d1da0588b81e9739981fe6d27caff
ogsahil <contactsahilpnayak@gmail.com>
2024-01-10 00:26:35 +0530

    added some changes

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hey this is my first project on Django!")
